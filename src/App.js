2c41fa98e231c7d44c343c7d47be2054fe931c1e
frontendprof <https://github.com/frontendprof>
2021-06-25 21:41:38 +0500

    first bundle of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react"
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from "./components/Header"
+import Main from "./components/Main"
+import StepTwo from "./components/StepTwo"
+import StepThree from "./components/StepThree"
+import Result from "./components/Result"
 
 function App() {
   return (
@@ -10,7 +15,7 @@ function App() {
         <Route exact path="/" component={Main} />
         <Route path="/step2" component={StepTwo} />
         <Route path="/step3" component={StepThree} />
-        <Route path="/step4" component={StepFour} />
+        <Route path="/result" component={Result} />
       </Switch>
     </Router>
     </>
---
+++ src/components/Result.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Result = () => {
+    return (
+        <div>
+            <h4>Hello from Result</h4>
+        </div>
+    )
+}
+
+export default Result

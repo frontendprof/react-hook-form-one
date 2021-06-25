53f51decea789f450d12ddc6f0e9ff4a48dda2cc
frontendprof <https://github.com/frontendprof>
2021-06-25 22:22:59 +0500

    Header component style one

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles=makeStyles(theme=>({
+    root:{
+        margin:theme.spacing(3,0,2),
+        textAlign:"center",
+        fontSize:"40px",
+        color:"orangered",
+        textShadow:"1px 1px darkmagenta",
+        fontFamily:"Permanent Marker"
+
+    }
+}))
 
 const Header = () => {
+    const classes=useStyles();
     return (
         <div>
-           <h1>
-                Hello from Header 
-            </h1>
+           <Typography component="h1" variant="h5" className={classes.root}>
+                REact Hook Form Learning
+           </Typography>
         </div>
     )
 }

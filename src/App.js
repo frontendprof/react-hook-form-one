9cd4a80e6de5e4af9a497b79a2fad1a0f71709d0
frontendprof <https://github.com/frontendprof>
2021-06-25 21:33:37 +0500

    Configuring routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react"
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+    <Header />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/step2" component={StepTwo} />
+        <Route path="/step3" component={StepThree} />
+        <Route path="/step4" component={StepFour} />
+      </Switch>
+    </Router>
+    </>
   );
 }
 

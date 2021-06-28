ac962dd82d4412057690da8236d79c8a7e890efe
frontendprof <https://github.com/frontendprof>
2021-06-28 10:29:23 +0500

    Assigning default values and creating table for received data

--- src/components/StepThree.js
+++ src/components/StepThree.js
@@ -4,41 +4,35 @@ import Typography from '@material-ui/core/Typography'
 import {useForm} from "react-hook-form"
 import { MainContainer } from './MainContainer'
 import { Form } from './Form'
-// import { Input } from './Input'
 import { PrimaryButton } from './PrimaryButton'
-// import * as yup from 'yup';
-// import { yupResolver } from '@hookform/resolvers/yup';
 import { useHistory } from 'react-router-dom'
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import parsePhoneNumberFromString from 'libphonenumber-js'
 import {FileInput} from "./FileInput"
+import { useData } from '../DataContext'
 
-
-
-
-// const schema=yup.object().shape({
-//     email:yup
-//     .string()
-//     .email("Email should have correct format")
-//     .required("Email is a required field")
-// })
 
 
 const StepThree = () => {
     const history=useHistory();
-    const {control,handleSubmit}=useForm();
+    const {data,setValues}=useData()
+    const {control,handleSubmit}=useForm({
+        defaultValues:{
+            files:data.files
+        }
+    });
 
 
 
     const onSubmit=(data)=>{
         history.push("/result")
+        setValues(data)
     }
     return (
         <MainContainer>
             <Typography component="h2" variant="h5">😀 Step 3</Typography>
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <FileInput name="files" control={control}/>
+
+
                 <PrimaryButton>Next</PrimaryButton>
             </Form>
 
